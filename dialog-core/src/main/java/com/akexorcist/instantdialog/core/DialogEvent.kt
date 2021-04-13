74ba222f375f42f75113cef66729b93d9d0a1aeb
Akexorcist <akexorcist@gmail.com>
2021-04-14 02:53:01 +0700

    Remove unused import

--- dialog-core/src/main/java/com/akexorcist/instantdialog/core/DialogEvent.kt
+++ dialog-core/src/main/java/com/akexorcist/instantdialog/core/DialogEvent.kt
@@ -1,9 +1,7 @@
 package com.akexorcist.instantdialog.core
 
 import android.os.Bundle
-import android.os.Parcelable
 import androidx.fragment.app.DialogFragment
-import kotlinx.android.parcel.Parcelize
 
 class DialogEvent(
     var dialog: DialogFragment,
